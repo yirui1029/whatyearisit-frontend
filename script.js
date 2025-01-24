2f1e7f762060c77d1cf60ec3701159c628d39402
yirui Dupuy <yirui.hao@gmail.com>
2025-01-24 10:58:40 +0100

    frontend

--- script.js
+++ script.js
@@ -1,5 +1,5 @@
 
-fetch('https://whatyearisit-backend-red-gamma.vercel.app/')
+fetch('https://whatyearisit-backend-red-gamma.vercel.app/year')
 .then(response => response.json())
 .then(data => {
     console.log(data);
